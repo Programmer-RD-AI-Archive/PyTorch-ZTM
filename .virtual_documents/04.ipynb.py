44de93bb69a21e18887a9b1d8dcd75e85f0bfbaa
Programmer-RD-AI <go2ranuga@gmail.com>
2023-07-06 05:30:28 +0530

    rdm
    Co-Authored-By: Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>

--- .virtual_documents/04.ipynb.py
+++ .virtual_documents/04.ipynb.py
@@ -33,5 +33,122 @@ def walk_through_dir(dir_path):
 walk_through_dir(data_path)
 
 
+
+
+
 import random
 from PIL import Image
+image_path_list = list(data_path.glob("*/*/*.jpg"))
+random_image_path = random.choice(image_path_list)
+# str(random_image_path).split("/")[-2]
+image_class =  random_image_path.parent.stem
+img = Image.open(random_image_path)
+# 5. Print metadata
+img
+
+
+random_image_path,image_class,img.height,img.width,
+
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+plt.figure(figsize=(10,7))
+plt.imshow(np.asarray(img))
+plt.axis(False);
+
+
+import torch
+from torch.utils.data import DataLoader
+from torchvision import datasets,transforms
+
+
+data_transform = transforms.Compose([
+    transforms.Resize(size=(128,128)),
+    transforms.RandomHorizontalFlip(p=0.5),
+    transforms.ToTensor()
+])
+
+
+plt.imshow(torch.permute(data_transform(img),(1,2,0)))
+
+
+def plot_transformed_images(image_paths,transform,n=3,seed=42):
+    """
+    Selects random iamges  from a pth of images and loads/transforms
+    them then plots the original vs the transformed version
+    """
+    random_image_paths = random.sample(image_paths,k=n)
+    for image_path in random_image_paths:
+        with Image.open(image_path) as f:
+            fig,ax = plt.subplots(1,2)
+            ax[0].imshow(f)
+            ax[0].axis(False)
+            ax[1].imshow(torch.permute(transform(f),(1,2,0)))
+            ax[1].axis(False)
+            fig.suptitle(f"Class : {image_path.parent.stem}",fontsize=16)
+
+
+plot_transformed_images(image_path_list,data_transform)
+
+
+train_dir = "data/04/01/train/"
+test_dir = "data/04/01/test/"
+
+
+# Use ImageFolder to create dataset(s)
+train_data = datasets.ImageFolder(root=train_dir,transform=data_transform)
+test_data = datasets.ImageFolder(root=test_dir,transform=data_transform)
+
+
+train_data,test_data
+
+
+# Get class names
+class_names = train_data.classes
+class_names
+
+
+class_dict = train_data.class_to_idx
+class_dict
+
+
+img,label = train_data[0][0],train_data[0][1]
+
+
+img.shape,img.dtype
+
+
+label,type(label)
+
+
+img_permute = img.permute(1,2,0)
+
+
+plt.imshow(img_permute)
+
+
+import os
+os.cpu_count()
+
+
+train_dataloader = DataLoader(train_data,batch_size=32,shuffle=True,num_workers=os.cpu_count())
+test_dataloader = DataLoader(test_data,batch_size=32,shuffle=True,num_workers=os.cpu_count())
+
+
+train_dataloader,test_dataloader
+
+
+
+img,label = next(iter(train_dataloader))
+
+
+img.shape
+
+
+plt.imshow(img[0].permute(1,2,0))
+
+
+
